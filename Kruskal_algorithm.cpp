c9f6d97c1e07201527ad95127198a42df8eb9e21
Bogdan <Semenov.BV@phystech.edu>
2019-10-20 18:55:42 +0300

    P

--- Kruskal_algorithm.cpp
+++ Kruskal_algorithm.cpp
@@ -33,7 +33,7 @@ class Graph {
     int weight;
 
     Edge(const Vertex &from, const Vertex &to, int weight) :
-        from(from), to(to), weight(weight) {}
+        from(std::min(from, to)), to(std::max(to, from)), weight(weight) {}
 
     bool operator<(const Edge &other) const {
       return weight < other.weight;
@@ -52,6 +52,8 @@ class Graph {
   virtual void AddEdge(const Vertex &start, const Vertex &finish, int weight) = 0;
 
   virtual std::vector<Edge> GetEdges() const = 0;
+
+  virtual std::vector<Vertex> GetAllVertices() const = 0;
 };
 
 class GraphAdjList : public Graph {
@@ -77,65 +79,73 @@ class GraphAdjList : public Graph {
   std::vector<Edge> GetEdges() const override {
     return edges_;
   }
+
+  std::vector<Vertex> GetAllVertices() const override {
+    std::vector<Vertex> all_vertices;
+    for (Vertex vertex = 1; vertex <= vertex_count_; ++vertex) {
+      all_vertices.push_back(vertex);
+    }
+    return all_vertices;
+  }
 };
 
 namespace GraphProcessing {
 
-template<typename T>
-class DSU {
- private:
-  std::map<T, size_t> rank_;
-  std::map<T, T> predecessors_;
+  template<typename T>
+  class DSU {
+   private:
+    std::map<T, size_t> rank_;
+    std::map<T, T> predecessors_;
 
-  T FindSet(const T &x) {
-    if (x == predecessors_[x]) {
-      return x;
+    T FindSet(const T &x) {
+      if (x == predecessors_[x]) {
+        return x;
+      }
+      return predecessors_[x] = FindSet(predecessors_[x]);
     }
-    return predecessors_[x] = FindSet(predecessors_[x]);
+
+   public:
+    explicit DSU(const std::vector<T> &elements) {
+      for (int i = 0; i < elements.size(); ++i) {
+        predecessors_[elements[i]] = elements[i];
+        rank_[elements[i]] = 0;
+      }
+    }
+
+    void Union(const T &x, const T &y) {
+      T x_predecessor = FindSet(x);
+      T y_predecessor = FindSet(y);
+
+      if (x_predecessor == y_predecessor) {
+        return;
+      }
+      if (rank_[x_predecessor] < rank_[y_predecessor]) {
+        std::swap(x_predecessor, y_predecessor);
+      }
+      predecessors_[y_predecessor] = x_predecessor;
+      if (rank_[x_predecessor] == rank_[y_predecessor]) {
+        ++rank_[x_predecessor];
+      }
+    }
+
+    bool InSameSet(const T &x, const T &y) {
+      return FindSet(x) == FindSet(y);
+    }
+  };
+
+  std::vector<Graph::Edge> Kruskal_Get_MST(const Graph &graph) {
+    auto edges = graph.GetEdges();
+    std::sort(edges.begin(), edges.end());
+    std::vector<Graph::Edge> min_span_tree;
+    DSU<Graph::Vertex> dsu(graph.GetAllVertices());
+    for (auto edge : edges) {
+      if (!dsu.InSameSet(edge.from, edge.to)) {
+        dsu.Union(edge.to, edge.from);
+        min_span_tree.push_back(edge);
+      }
+    }
+    return min_span_tree;
   }
-
- public:
-  explicit DSU(const T &start, const T &finish) {
-    for (T set = start; set <= finish; ++set) {
-      predecessors_[set] = set;
-      rank_[set] = 0;
-    }
-  }
-
-  void Union(const T &x, const T &y) {
-    T x_predecessor = FindSet(x);
-    T y_predecessor = FindSet(y);
-
-    if (x_predecessor == y_predecessor) {
-      return;
-    }
-    if (rank_[x_predecessor] < rank_[y_predecessor]) {
-      std::swap(x_predecessor, y_predecessor);
-    }
-    predecessors_[y_predecessor] = x_predecessor;
-    if (rank_[x_predecessor] == rank_[y_predecessor]) {
-      ++rank_[x_predecessor];
-    }
-  }
-
-  bool InSameSet(const T &x, const T &y) {
-    return FindSet(x) == FindSet(y);
-  }
-};
-
-std::vector<Graph::Edge> Kruskal_Get_MST(const Graph &graph) {
-  auto edges = graph.GetEdges();
-  std::sort(edges.begin(), edges.end());
-  std::vector<Graph::Edge> min_span_tree;
-  DSU<int> dsu(1, graph.GetVertexCount());
-  for (auto edge : edges) {
-    if (!dsu.InSameSet(edge.from, edge.to)) {
-      dsu.Union(edge.to, edge.from);
-      min_span_tree.push_back(edge);
-    }
-  }
-  return min_span_tree;
-}
 }
 
 int main() {
