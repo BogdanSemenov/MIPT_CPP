b8cdd701126ea4cffdf9ca1398d4de29a6cbf47a
Bogdan <Semenov.BV@phystech.edu>
2019-11-24 17:09:49 +0300

    Contest 2 Task H

--- H.cpp
+++ H.cpp
@@ -1,53 +1,63 @@
 #include <iostream>
 #include <vector>
 
-template <typename T>
+struct Point {
+  int64_t x_;
+  int64_t y_;
+
+  Point(int64_t x, int64_t y) : x_(x), y_(y) {}
+};
+
+template<typename T>
 class FenwickTree_2D {
  private:
   std::vector<std::vector<T>> tree_;
 
-  T RSQ(int64_t x, int64_t y) const;
+  T RSQ(const Point &point) const;
  public:
   explicit FenwickTree_2D(size_t height, size_t width);
-  T RSQ(int64_t x1, int64_t y1, int64_t x2, int64_t y2) const;
-  void Update(size_t x, size_t y, const T &value);
+  T RSQ(const Point &lhs, const Point &rhs) const;
+  void Update(const Point &point, const T &value);
   size_t GetWidth() const;
   size_t GetHeight() const;
 };
 
-template <typename T>
+template<typename T>
 FenwickTree_2D<T>::FenwickTree_2D(size_t height, size_t width) : tree_(height, std::vector<T>(width)) {}
 
-template <typename T>
-T FenwickTree_2D<T>::RSQ(int64_t x, int64_t y) const {
+template<typename T>
+T FenwickTree_2D<T>::RSQ(const Point &point) const {
   T sum = 0;
-  for(int64_t i = x; i >= 0; i = ((i & (i + 1)) - 1)) {
-    for (int64_t j = y; j >= 0; j = ((j & (j + 1)) - 1)) {
+  for (int64_t i = point.x_; i >= 0; i = ((i & (i + 1)) - 1)) {
+    for (int64_t j = point.y_; j >= 0; j = ((j & (j + 1)) - 1)) {
       sum += tree_[i][j];
     }
   }
   return sum;
 }
 
-template <typename T>
-T FenwickTree_2D<T>::RSQ(int64_t x1, int64_t y1, int64_t x2, int64_t y2) const {
-  return RSQ(x2, y2) - RSQ(x1 - 1, y2) - RSQ(x2, y1 - 1) + RSQ(x1 - 1, y1 - 1);
+template<typename T>
+T FenwickTree_2D<T>::RSQ(const Point &lhs, const Point &rhs) const {
+  return RSQ(Point(rhs.x_, rhs.y_)) -
+         RSQ(Point(lhs.x_ - 1, rhs.y_)) -
+         RSQ(Point(rhs.x_, lhs.y_ - 1)) +
+         RSQ(Point(lhs.x_ - 1, lhs.y_ - 1));
 }
 
-template <typename T>
+template<typename T>
 size_t FenwickTree_2D<T>::GetWidth() const {
   return tree_[0].size();
 }
 
-template <typename T>
+template<typename T>
 size_t FenwickTree_2D<T>::GetHeight() const {
   return tree_.size();
 }
 
-template <typename T>
-void FenwickTree_2D<T>::Update(size_t x, size_t y, const T &value) {
-  for (size_t i = x; i < GetHeight(); i = (i | (i + 1))) {
-    for (size_t j = y; j < GetWidth(); j = (j | (j + 1))) {
+template<typename T>
+void FenwickTree_2D<T>::Update(const Point &point, const T &value) {
+  for (size_t i = point.x_; i < GetHeight(); i = (i | (i + 1))) {
+    for (size_t j = point.y_; j < GetWidth(); j = (j | (j + 1))) {
       tree_[i][j] += value;
     }
   }
@@ -65,11 +75,11 @@ int main() {
       size_t x1, y1;
       int64_t value;
       std::cin >> x1 >> y1 >> value;
-      fenwick_tree.Update(x1 - 1, y1 - 1, value);
+      fenwick_tree.Update(Point(x1 - 1, y1 - 1), value);
     } else if (query == 2) {
       int64_t x1, y1, x2, y2;
       std::cin >> x1 >> y1 >> x2 >> y2;
-      std::cout << fenwick_tree.RSQ(x1 - 1, y1 - 1, x2 - 1, y2 - 1) << std::endl;
+      std::cout << fenwick_tree.RSQ(Point(x1 - 1, y1 - 1), Point(x2 - 1, y2 - 1)) << std::endl;
     }
   }
 
