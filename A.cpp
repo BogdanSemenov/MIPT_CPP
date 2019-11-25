a06b9b4655c67fa6a3b0c5041d52fc4598649c34
Bogdan <Semenov.BV@phystech.edu>
2019-11-25 14:55:45 +0300

    Contest 2 Task A

--- A.cpp
+++ A.cpp
@@ -15,31 +15,26 @@ class SparseTable {
  private:
   std::vector<std::vector<T>> sparse_table_;
   Function function_;
-  std::vector<int> log_2_;
+  std::vector<size_t> log_2_;
 
  public:
-  explicit SparseTable(const std::vector<T> &values) : log_2_(values.size() + 1, 0) {
-    const int table_size = values.size();
-    for (int i = 2; i <= table_size; ++i) {
+  explicit SparseTable(const std::vector<T> &values) : log_2_(values.size(), 0) {
+    const size_t table_size = values.size();
+    for (size_t i = 2; i < table_size; ++i) {
       log_2_[i] = log_2_[i / 2] + 1;
     }
-    sparse_table_.resize(log_2_[table_size] + 1, std::vector<T>(table_size + 1));
-    for (int i = 0; i <= log_2_[table_size]; ++i) {
-      for (int j = 0; j < table_size; ++j) {
-        if (i == 0) {
-          sparse_table_[i][j] = values[j];
-        } else {
-          sparse_table_[i][j] = function_(sparse_table_[i - 1][j], sparse_table_[i - 1][j + (1u << (i - 1))]);
-        }
+    sparse_table_.push_back(values);
+    for (size_t delta = 2; delta <= table_size; delta *= 2) {
+      std::vector<T> next_level_values(table_size - delta + 1);
+      for (size_t i = 0; i < next_level_values.size(); ++i) {
+        next_level_values[i] = function_(sparse_table_.back()[i], sparse_table_.back()[i + delta / 2]);
       }
+      sparse_table_.push_back(next_level_values);
     }
   }
 
-  T Query(int left, int right) const {
-    if (left > right) {
-      std::swap(left, right);
-    }
-    int log_2 = log_2_[right - left];
+  T Query(size_t left, size_t right) const {
+    size_t log_2 = log_2_[right - left];
     return function_(sparse_table_[log_2][left], sparse_table_[log_2][right - (1u << log_2) + 1]);
   }
 };
@@ -52,27 +47,24 @@ struct MakeValue {
 };
 
 int main() {
-  int value_num, queries_num;
-  int first_value;
-  std::ios_base::sync_with_stdio(false);
-  std::cin.tie(nullptr);
-  std::cout.tie(nullptr);
+  size_t value_num, queries_num, first_value;
   std::cin >> value_num >> queries_num >> first_value;
-  std::vector<int> values(value_num);
+  std::vector<size_t> values(value_num);
   values[0] = first_value;
-  for (int i = 1; i < value_num; ++i) {
-    values[i] = MakeValue<int>::MakeNewValue(values[i - 1]);
+  for (size_t i = 1; i < value_num; ++i) {
+    values[i] = MakeValue<size_t>::MakeNewValue(values[i - 1]);
   }
-  SparseTable<int, Min<int>> sparse_table(values);
-  int left, right;
+  SparseTable<size_t, Min<size_t>> sparse_table(values);
+  size_t left, right;
   std::cin >> left >> right;
-  int ans = 0;
-  for (int i = 1; i < queries_num; ++i) {
-    ans = sparse_table.Query(left - 1, right - 1);
-    left = (17 * left + 751 + ans + 2 * i) % value_num + 1;
-    right = (13 * right + 593 + ans + 5 * i) % value_num + 1;
+  size_t answer = 0;
+  for (size_t i = 1; i < queries_num; ++i) {
+    answer = sparse_table.Query(std::min(left - 1, right - 1), std::max(left - 1, right - 1));
+    left = (17 * left + 751 + answer + 2 * i) % value_num + 1;
+    right = (13 * right + 593 + answer + 5 * i) % value_num + 1;
   }
-  std::cout << left << ' ' << right << ' ' << sparse_table.Query(left - 1, right - 1);
+  answer = sparse_table.Query(std::min(left - 1, right - 1), std::max(left - 1, right - 1));
+  std::cout << left << ' ' << right << ' ' << answer;
 
   return 0;
 }
